55069c37b6f0d2bd4425d9b25ef67be92358a297
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-22 18:25:11 +0300

    Добавьте файлы проекта.

---
+++ Homework_37/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player("forofonof", 15, 125, 50);
+
+        player.ShowInfo();
+    }
+}
+
+class Player
+{
+    public string Nickname;
+    public int Level;
+    public int Health;
+    public int Damage;
+
+    public Player(string nickname, int level, int health, int damage)
+    {
+        Nickname = nickname;
+        Level = level;
+        Health = health;
+        Damage = damage;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Никнейм: {Nickname}.\nУровень: {Level}.\nЗдоровье: {Health}.\nУрон: {Damage}.");
+    }
+}
